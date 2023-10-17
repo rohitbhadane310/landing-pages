a10cd5a9655421a4e30e3f5b14a3e80030dd07f0
Rohit Bhadane <rohitbhadane310@gmail.com>
2023-10-17 18:10:25 +0530

    working on footer

--- resto-web/src/components/Footer.jsx
+++ resto-web/src/components/Footer.jsx
@@ -1,9 +1,94 @@
-import React from 'react'
+import React from "react";
+import { BsFacebook } from "react-icons/bs";
+import { RiTwitterXFill } from "react-icons/ri";
+import { BsInstagram } from "react-icons/bs";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <div className=" bg-[#514e55] text-white rounded-t-3xl">
+      <div className="flex flex-col md:flex-row justify-between p-8 md:px-32 px-5">
+        <div className=" w-full md:w-1/4">
+          <h1 className=" font-semibold text-xl pb-4">FoodieWeb</h1>
+          <p className=" text-sm">
+            Indulge in a symphony of flavors, where each plate is a canvas for
+            culinary excellence.
+          </p>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Links</h1>
+          <nav className=" flex flex-col gap-2">
+            <a
+              className=" hover:text-brightColor transition-all cursor-pointer"
+              href="/"
+            >
+              Dishes
+            </a>
+            <a
+              className=" hover:text-brightColor transition-all cursor-pointer"
+              href="/"
+            >
+              About
+            </a>
+            <a
+              className=" hover:text-brightColor transition-all cursor-pointer"
+              href="/"
+            >
+              Menu
+            </a>
+            <a
+              className=" hover:text-brightColor transition-all cursor-pointer"
+              href="/"
+            >
+              Reviews
+            </a>
+          </nav>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Menu</h1>
+          <nav className=" flex flex-col gap-2">
+            <a
+              className=" hover:text-brightColor transition-all cursor-pointer"
+              href="/"
+            >
+              Our Dishes
+            </a>
+            <a
+              className=" hover:text-brightColor transition-all cursor-pointer"
+              href="/"
+            >
+              Premium Menu
+            </a>
+          </nav>
+        </div>
+        <div>
+          <h1 className=" font-medium text-xl pb-4 pt-5 md:pt-0">Follow Us</h1>
+          <nav className=" flex flex-col md:items-center gap-2">
+            <BsFacebook
+              size={25}
+              className="hover:text-brightColor transition-all cursor-pointer"
+            />
+            <RiTwitterXFill
+              size={25}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            />
+            <BsInstagram
+              size={25}
+              className=" hover:text-brightColor transition-all cursor-pointer"
+            />
+          </nav>
+        </div>
+      </div>
+      <div>
+        <p>
+          <p className=" text-center py-4">
+            @copyright developed by
+            <span className=" text-brightColor"> champion programmers</span> |
+            All rights reserved
+          </p>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Footer
+export default Footer;
